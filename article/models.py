ec5183074d8978987286e8b36991fc6d704a200b
Александр Смолин <smolll123@mail.ru>
2022-04-15 19:37:22 +0300

    create models, add to admin

--- article/models.py
+++ article/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    content = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return str(self.content)
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, related_name='comments', on_delete=models.CASCADE)
+    content = models.CharField(max_length=150)
+    parent = models.ForeignKey('self', related_name='replies', blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.content)
