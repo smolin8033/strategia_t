1c0d6282ff13263673351a77cca426150dab6d52
Александр Смолин <smolll123@mail.ru>
2022-04-16 22:17:00 +0300

    comment levels written

--- article/views.py
+++ article/views.py
@@ -1,7 +1,8 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
-from .models import Article
+from .models import Article, Comment
 from .serializers import (
     ArticleSerializer,
     CommentSerializer,
@@ -24,6 +25,15 @@ class CommentCreateView(CreateAPIView):
     or other comments to
     """
     serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        created_comment = serializer.save()
+        if created_comment.parent is None:
+            created_comment.level = 1
+        else:
+            created_comment.level = created_comment.parent.level + 1
+        created_comment.save()
+        return created_comment
 
 
 class ArticleWithCommentsDetailView(RetrieveAPIView):
