3a8e02f921d13a8dbf87765c9c0f1d8bc81a4876
Александр Смолин <smolll123@mail.ru>
2022-04-18 12:31:09 +0300

    add LOGGING to settings.py, delete unnecessary imports from views

--- article/views.py
+++ article/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
-from rest_framework.response import Response
 from .models import Article, Comment
 from .serializers import (
     ArticleSerializer,
